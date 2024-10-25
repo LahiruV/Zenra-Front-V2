c978673ea394d9c156da728e6123522a9b6b7d08
LahiruVV <lahiruvimukthi292@gmail.com>
2024-10-25 23:26:53 +0530

    Refactor font imports and remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
